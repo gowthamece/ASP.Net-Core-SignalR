39bcbf7e1223c51abdc2880e5d7abdb973eedb76
Gowtham K <gowthamk91@outlook.com>
2020-02-09 22:43:15 +0530

    Add project files.

---
+++ SignalRCoreApp/Hub/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRCoreApp.Hub
+{
+    [Authorize]
+    public class Chat: Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",Context.User.Identity.Name?? "anonymous", message);
+        }
+    }
+}
---
+++ SignalRCoreApp/wwwroot/js/chat.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/Chat").build();
+
+//Disable send button until connection is established
+document.getElementById("sendBtn").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("ulmessages").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendBtn").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendBtn").addEventListener("click", function (event) {    
+    var message = document.getElementById("txtmessage").value;
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
